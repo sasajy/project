1ac126419c6fe9583cadcede065188fc52e15fef
sasajy <128241767+sasajy@users.noreply.github.com>
2025-06-09 13:33:07 +0900

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,11 +1,12 @@
 import streamlit as st
+from datetime import datetime
 
 def render_task_card(task, index, key_prefix):
     done = task.get("done", False)
     name = task.get("name", "No Name")
     detail = task.get("detail", "")
     key_done = f"{key_prefix}_done_{index}"
-    key_button = f"{key_prefix}_button_{index}"
+    
     # 現在の状態をセッションから取得
     if key_done not in st.session_state:
         st.session_state[key_done] = task.get("done", False)
@@ -22,3 +23,29 @@ def render_task_card(task, index, key_prefix):
     # Checkbox reflecting task done status
 
     return done
+
+def render_task_card1(task, index, key_prefix):
+    done = task.get("done", False)
+    name = task.get("name", "No Name")
+    done_by = task.get("done_by", "")
+    detail = task.get("detail", "")
+    key_done = f"{key_prefix}_done_{index}"
+    
+    # 現在の状態をセッションから取得
+    if key_done not in st.session_state:
+        st.session_state[key_done] = task.get("done", False)
+
+    cols = st.columns([0.1, 0.9])
+    tf = 0
+    with cols[0]:
+        done = st.checkbox("",value=st.session_state.get(key_done, False), key=key_done)
+
+    with cols[1]:
+        st.write(f"{name} — {done_by}")
+
+    # Checkbox reflecting task done status
+
+    return done
+
+
+
